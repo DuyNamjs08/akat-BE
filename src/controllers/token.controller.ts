5bb472d2dff6d0e6caeb661612be06ff0109a511
nguyenthanhdat <nguyend@digitalplatformer.co.j>
2025-05-06 17:31:03 +0700

    ud-db1

--- src/controllers/token.controller.ts
+++ src/controllers/token.controller.ts
@@ -10,6 +10,7 @@ const TokenController = {
   createAccessToken: async (req: Request, res: Response): Promise<void> => {
     try {
       const data = req.body;
+      console.log(data);
       const user = await UserService.getUserByEmail(data.email);
       if (!user) {
         errorResponse(
